1b5d5448765615b84279cd3485fd5c7161a4bded
the-camputer <mynichor@gmail.com>
2023-08-01 21:04:31 -0400

    gh-21 Added formatting to Home page and basic render check test

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack, Button, Typography } from '@mui/joy';
+import styled from 'styled-components';
+
+const Background = styled.div`
+  background-image: url('background.gif');
+  background-origin: center;
+  background-repeat: no-repeat;
+  background-size: 100vw 100vh;
+`;
 
 const Home: React.FC = (props: any) => {
-  return <div>Sup, Earth!</div>;
+  return (
+    <Background>
+      <Stack height='100vh' justifyContent='space-around'>
+        <Typography
+          level='h1'
+          sx={{
+            color: 'white',
+            WebkitTextStroke: '1px black',
+            fontSize: '12em',
+          }}
+        >
+          Game of Life
+        </Typography>
+        <Link to={'/game'}>
+          <Button size='lg'>Play</Button>
+        </Link>
+      </Stack>
+    </Background>
+  );
 };
 
 export default Home;
